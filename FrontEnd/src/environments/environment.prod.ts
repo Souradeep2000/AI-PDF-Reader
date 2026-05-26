0e096841afacdc98df54d0f5f68be92d7b66f1ee
souradeep2000 <souradeepgharami2000@gmail.com>
2026-05-26 13:16:05 +0530

    Update frontend for deployed backend

--- FrontEnd/src/environments/environment.prod.ts
+++ FrontEnd/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  apiBaseUrl: 'https://Souro2000-pdf-qna-backend.hf.space',
+  apiBaseUrl: 'https://Souro2000-pdf-qna-backend.hf.space/api',
 };
 
 // ng build --configuration production
